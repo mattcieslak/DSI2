6328b0d19ce57b8d5344ad70567d71645c0d86a8
mattcieslak <mattcieslak@gmail.com>
2015-05-03 17:51:50 -0700

    cleaned up setup.py

--- setup.py
+++ setup.py
@@ -3,34 +3,25 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
+# Version
+MAJOR="0"
+MINOR="5"
+MICRO="0"
+VERSION= MAJOR + "." + MINOR + "." + MICRO
+
 here = path.abspath(path.dirname(__file__))
 with open(path.join(here,'README.md')) as f:
     long_description = f.read()
 
 setup(  name='DSI2',
-        version='0.2.0',
+        version=VERSION,
         description='DSI2 Toolbox',
         author='Matthew Cieslak',
         author_email='mattcieslak@gmail.com',
         license='GPLv3',
         url='https://github.com/mattcieslak/DSI2',
-        packages = find_packages( exclude = ['doc', 'tests']),
-        include_package_data = True,
-        package_data={
-            "dsi2":[
-                "dsi2/example_data/lausanne2008/ParcellationLausanne2008.xls",
-                "dsi2/example_data/lausanne2008/README.txt",
-                "dsi2/example_data/lausanne2008/resolution1015/resolution1015.graphml",
-                "dsi2/example_data/lausanne2008/resolution150/resolution150.graphml",
-                "dsi2/example_data/lausanne2008/resolution258/resolution258.graphml",
-                "dsi2/example_data/lausanne2008/resolution500/resolution500.graphml",
-                "dsi2/example_data/lausanne2008/resolution83/resolution83.graphml",
-                "dsi2/example_data/MNI152_T1_2mm_brain_mask.nii.gz",
-                "dsi2/example_data/MNI152_T1_2mm.nii.gz",
-                "dsi2/example_data/MNI_BRAIN_MASK_FLOAT.nii.gz",
-                "dsi2/example_data/NTU90_QA.nii.gz"
-            ]
-        },
+        packages = find_packages( exclude = ['doc', 'tests', "example_data"]),
+        include_package_data = True, # Comes from MANIFEST.in
         entry_points = {
             'gui_scripts':[
                 'dsi2_browse = dsi2.app_launch:browser_builder',
