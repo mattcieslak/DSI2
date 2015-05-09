1060bda2c03ea8d37e095fd75e814501ea40122f
mattcieslak <mattcieslak@gmail.com>
2015-05-08 22:44:34 -0700

    optimize but leave in debug objects

--- py2app.sh
+++ py2app.sh
@@ -2,7 +2,7 @@
 
 # add TVTK classes zip file to library; fixes: 'ImportError: TVTK not built properly. Unable to find either a directory: /home/jvb/git/infobiotics-dashboard/dist/library.zip/tvtk/tvtk_classes or a file: /home/jvb/git/infobiotics-dashboard/dist/library.zip/tvtk/tvtk_classes.zip with the TVTK classes.'
 echo "running py2app"
-python setupApp.py py2app
+python setupApp.py py2app -O1
 if [ ! -d dist/DSI2.app ]; then
 echo ERROR no app built
 exit 1
@@ -18,3 +18,5 @@ mkdir -p dist/DSI2.app/Contents/Resources/dsi2
 cp -r dsi2/example_data dist/DSI2.app/Contents/Resources/dsi2/
 cp -r /usr/local/lib/python2.7/site-packages/sklearn dist/DSI2.app/Contents/Resources/lib/python2.7/site-packages
 
+cd dist
+tar cvfz DSI2.app.tar.gz DSI2.app
